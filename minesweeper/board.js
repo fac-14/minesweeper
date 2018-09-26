59b5131fc5a1d44d554d57abce5e8040ab943977
sima-qian <29804933+sima-qian@users.noreply.github.com>
2018-09-26 14:57:50 +0100

    add random number generator

--- minesweeper/board.js
+++ minesweeper/board.js
@@ -1,15 +1,33 @@
 import React from "react";
 import Tile from "./tile";
+import getRandomInt from "../utils/random-number";
 // import some funcationality some utils
 
 class Board extends React.Component {
   state = {
-    // mines
     boardArray: this.createBoardArray(16, 30)
   };
+
   createBoardArray(x, y) {
     let newArray = Array(x).fill(undefined);
     return newArray.map(() => Array(y).fill(new Tile()));
+  }
+
+  initialiseBoard() {
+    let mines = 0;
+    while (mines < 99) {
+      // get random x, y numbers (range 0-99)
+      let x = getRandomInt(99),
+        y = getRandomInt(99);
+      console.log(x, y);
+      // check that tile for mine
+      // if no mine:
+      //   place mine
+      //   for all surrounding tiles, if not mine add 1 to value
+      //   mines++
+      // else:
+      //   loop again
+    }
   }
 
   // createBoard():
